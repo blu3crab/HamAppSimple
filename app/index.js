43df5fe6b45d1b24c3ef3f5a6529609a75f3c105
matucker <matucker@adaptivehandyapps.com>
2021-06-30 15:51:54 -0400

    minor update - no permission shows "NO" on clock face

---
+++ app/index.js
@@ -0,0 +1,8 @@
+//
+//  index.js by  M.A.Tucker 01JAN2021
+//  - Entry point for the watch app
+//
+import * as heartActivityMonitor from "./heartActivityMonitor.js"
+
+// start heart activity monitor (HAM)
+heartActivityMonitor.start();
